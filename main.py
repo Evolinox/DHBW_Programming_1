aab720dac33302bbf38f5422447cd9595bb346a4
Pascal Jedicke Surface9 <pascal.jedicke@icloud.com>
2023-12-08 09:01:14 +0100

    Basic Bot Setup

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import mathlib.heron
+# This example requires the 'message_content' intent.
 
-mathlib.heron.heronsMethod(input("Name a number: "))
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTE4MjU5MDU2MzY0NDYwNDQ5Ng.G8sHcn.AX7n1Pz25l4ZOR8_uhgUlRwyGiNWmBG2JlW8ck')
