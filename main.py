b5ad92ddcc0e4bd2f9aa1731385d5651712f7893
Pascal <72224389+Evolinox@users.noreply.github.com>
2023-12-09 17:13:51 +0100

    Preperations for Bot

--- main.py
+++ main.py
@@ -2,21 +2,18 @@
 
 import discord
 
+with open('token.txt') as file:
+    token = file.readlines()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('MTE4MjU5MDU2MzY0NDYwNDQ5Ng.G8sHcn.AX7n1Pz25l4ZOR8_uhgUlRwyGiNWmBG2JlW8ck')
+client = MyClient(intents=intents)
+client.run(token[0])
