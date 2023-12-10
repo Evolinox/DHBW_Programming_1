4e3b8dfbb9668994e2b84fff076e3427fdb9f932
Pascal <72224389+Evolinox@users.noreply.github.com>
2023-12-10 11:11:57 +0100

    Commit 10.12.23

---
+++ escalator/escalator.py
@@ -0,0 +1,51 @@
+import discord
+import random
+import json
+import os
+
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+class Escalator(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        client = discord.Client(intents=intents)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, after, before):
+
+        guild = member.guild
+
+        if not str(guild.id) == 1183077178003443873:
+            return
+        else:
+            print("aihwdoiahwdoiöaw")
+            channel_exists = True
+            channel_id = 1183077178003443873
+            channel_category = 1183077113708945438
+            if channel_id and channel_exists == True:
+                channel = discord.utils.get(guild.voice_channels, id = int(channel_id))
+
+                for user in channel.members:
+                    if user == member:
+                        user_name = member.display_name
+                        for channelTemp in guild.voice_channels:
+                            if channelTemp.name == (user_name + "'s Channel"):
+                                await channelTemp.delete()
+
+                        category = discord.utils.get(guild.categories, id=int(channel_category))
+                        await guild.create_voice_channel(user_name + "'s Channel", category = category)
+                        for channelTemp2 in guild.voice_channels:
+                            if channelTemp2.name == (user_name + "'s Channel"):
+                                await member.edit(voice_channel = channelTemp2)
+
+            temp = "'s Channel"
+            for channels in guild.voice_channels:
+                if temp in channels.name:
+                    if len(channels.members) == 0:
+                        await channels.delete()
+
+def setup(bot):
+    bot.add_cog(Escalator(bot))
--- main.py
+++ main.py
@@ -1,19 +1,46 @@
-# This example requires the 'message_content' intent.
-
+import time
+import json
+import datetime
+import time
 import discord
+import discord.utils
+import os
+import sys
 
+from sys import argv
+from discord.ext import commands
+from discord.utils import get
+from datetime import datetime
+
+# Lies den Token aus der Textdatei
 with open('token.txt') as file:
     token = file.readlines()
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(token[0])
+bot = commands.Bot(command_prefix="f!", intents=intents)
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="fahrstuhlmusik"))
+
+    for filename in os.listdir(os.path.join(os.path.dirname(__file__), './escalator')):
+        if filename.endswith('py'):
+            bot.load_extension(f'escalator.{filename[:-3]}')
+            print(f'DEBUG: loaded extension: {filename[:-3]}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('fahrstuhl'):
+        await message.channel.send('Ich bin der Fahrstuhl Bot, ich sorge für wunderschöne Musik, während ihr im Fahrstuhl chilled :) (Ich gehöre Pascal, falls es Probleme gibt)')
+
+    if message.content.startswith('Nett hier'):
+        await message.channel.send('...aber waren Sie schon mal in Baden-Württemberg?')
+
+bot.run(token[0])
