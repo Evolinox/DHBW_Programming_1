08a4ec504605d43374a8e113a467e21a27e6bbd6
Pascal <72224389+Evolinox@users.noreply.github.com>
2023-12-11 21:44:50 +0100

    Update

--- extensions/ollama-integration.py
+++ extensions/ollama-integration.py
@@ -1,14 +1,21 @@
 import discord
 
 from discord.ext import commands
+from discord import app_commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
 class Ollama(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        client = discord.Client(intents=intents)
+
+@tree.command(name = "ask", description = "Ask Llama something", guild=discord.Object(id=1179106176915480686))
+async def slash_command(interaction: discord.Interaction):
+    await interaction.response.send_message("you asked me something, but my LLM isnt working at the moment!")
 
 async def setup(bot):
     await bot.add_cog(Ollama(bot))
--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import discord.utils
 import os
 
 from discord.ext import commands
+from discord import app_commands
 
 # Lies den Token aus der Textdatei
 with open('token.txt') as file:
     token = file.readlines()
 
 # Setze die Intents
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
+
+# Commands Setup
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 # Command Setup
 bot = commands.Bot(command_prefix="f!", intents=intents)
@@ -19,6 +24,12 @@ bot.remove_command("help")
 @bot.event
 async def on_ready():
     print(f'DEBUG: logged in as {bot.user}')
+
+    # Commands (Funktioniert net, weil appid fehlt? Maybe bot durch client ändern, 
+    # dann machen Cogs aber probleme)
+    
+    # await tree.sync(guild=discord.Object(id=1179106176915480686))
+    print("DEBUG: synced Command Tree")
 
     # Setze die Aktivität vom Bot für das GUI
     await bot.change_presence(activity=discord.Game(name = "Mistral 7B"))
