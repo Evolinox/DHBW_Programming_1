0e4df6b92a84ea2eb27882d2f0f4262f7acf854b
Pascal <72224389+Evolinox@users.noreply.github.com>
2023-12-11 20:11:54 +0100

    Added ollama-integration

--- extensions/ollama-integration.py
+++ extensions/ollama-integration.py
@@ -5,10 +5,10 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-class Roles(commands.Cog):
+class Ollama(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         client = discord.Client(intents=intents)
 
 async def setup(bot):
-    await bot.add_cog(Roles(bot))
+    await bot.add_cog(Ollama(bot))
