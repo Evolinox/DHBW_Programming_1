664e60aff1a7fbe19f4bddb91c43af8e77406613
Pascal Jedicke Surface9 <pascal.jedicke@icloud.com>
2023-12-19 09:34:08 +0100

    Cleanup Slashcommand Manager

--- extensions/slashcommandsmanager.py
+++ extensions/slashcommandsmanager.py
@@ -1,7 +1,6 @@
 import discord
 
 from discord.ext import commands
-from discord import app_commands
 
 class SlashcommandManager(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -19,4 +18,4 @@ class SlashcommandManager(commands.Cog):
         print("DEBUG: synced Command Tree")
 
 async def setup(bot):
-    await bot.add_cog(SlashcommandManager(bot), guilds = [discord.Object(id=1179106176915480686), discord.Object(id=635480321541931029)])
+    await bot.add_cog(SlashcommandManager(bot), guilds = [discord.Object(id=1179106176915480686), discord.Object(id=635480321541931029), discord.Object(id=1115380293063430214)])
