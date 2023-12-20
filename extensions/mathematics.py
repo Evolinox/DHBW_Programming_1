57fa1de2a11cf3b7f696e995b39b91c6fb39b4b1
Pascal <72224389+Evolinox@users.noreply.github.com>
2023-12-20 15:12:07 +0100

    Updated mathematics classes

--- extensions/mathematics.py
+++ extensions/mathematics.py
@@ -1,6 +1,6 @@
 import discord
-import mathlib.vollstInduktion
-import mathlib.heron
+import mathlib.heron as heron
+import mathlib.vollstInduktion as induktion
 
 from discord.ext import commands
 from discord import app_commands
@@ -17,7 +17,7 @@ class Mathematics(commands.Cog):
     @app_commands.command(name = "induktion", description = "Beweise, ob die vollständige Induktion funktioniert")
     async def vollstInduktioninduktion(self, interaction: discord.Interaction, func: str):
         await interaction.response.defer()
-        res = mathlib.vollstInduktion.induktion(func)
+        res = induktion.induktion(func)
         if res == 0 :
             await interaction.followup.send("die vollständige Induktion " + func + " funktioniert")
         else:
@@ -26,8 +26,8 @@ class Mathematics(commands.Cog):
     # Slashbefehl um die Wurzel einer Zahl zu finden
     @app_commands.command(name = "sqrt", description = "Gibt die Wurzel einer beliebigen Zahl wieder")
     async def sqrt(self, interaction: discord.Interaction, number: int):
-        res = mathlib.heron.heronsMethod(number)
-        await interaction.response.send_message(f"Die Wurzel aus {number} ist: {res}")
+        res = heron.heronsMethod(number)
+        await interaction.response.send_message(f"Die Wurzel aus {number} ist: {res:3.2f}")
 
 async def setup(bot):
-    await bot.add_cog(Mathematics(bot), guilds = [discord.Object(id=1179106176915480686), discord.Object(id=635480321541931029)])
+    await bot.add_cog(Mathematics(bot))
