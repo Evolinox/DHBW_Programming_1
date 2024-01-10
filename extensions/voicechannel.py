29164bd20b7d888c8155ba10579d718ff4bdcd91
Pascal <72224389+Evolinox@users.noreply.github.com>
2024-01-10 17:57:53 +0100

    Worked on voicechannels

--- extensions/voicechannel.py
+++ extensions/voicechannel.py
@@ -20,5 +20,28 @@ class Voicechannel(commands.Cog):
         # Get current Guild
         guild = member.guild
 
+        if guild.id != devServerID:
+            print(f"Irgendwas stimmt net.")
+        else:
+            channel_exists = True
+            channel_id = createChannelID
+            channel_category = categoryID
+
+            if channel_id and channel_exists == True:
+                channel = discord.utils.get(guild.voice_channels, id = int(channel_id))
+
+                for user in channel.members:
+                    if user == member:
+                        user_name = member.display_name
+                        for channelTemp in guild.voice_channels:
+                            if channelTemp.name == (user_name + "'s Channel"):
+                                await channelTemp.delete()
+
+                        category = discord.utils.get(guild.categories, id=int(channel_category))
+                        await guild.create_voice_channel(user_name + "'s Channel", category = category)
+                        for channelTemp2 in guild.voice_channels:
+                            if channelTemp2.name == (user_name + "'s Channel"):
+                                await member.edit(voice_channel = channelTemp2)
+
 async def setup(bot):
     await bot.add_cog(Voicechannel(bot))
