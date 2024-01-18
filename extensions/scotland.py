a6d797d3ae0992ef005a957caaa39785ee30e755
Pascal <72224389+Evolinox@users.noreply.github.com>
2024-01-18 20:33:07 +0100

    Added Scotland Response

---
+++ extensions/scotland.py
@@ -0,0 +1,23 @@
+import discord
+
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+class Scotland(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        client = discord.Client(intents=intents)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        elif "scotland" in message.content.lower():
+            await message.reply("https://media.tenor.com/FGME0yGf0QcAAAAM/scotland-forever.gif")
+        else:
+            print("do nothin")
+
+async def setup(bot):
+    await bot.add_cog(Scotland(bot))
